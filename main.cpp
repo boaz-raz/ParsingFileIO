6424601f4d04a6f9349a415d06eb536f11c16ba3
Boaz Raz <raz.boaz@gmail.com>
2016-07-31 14:38:07 -0400

    fix working directory path

--- main.cpp
+++ main.cpp
@@ -17,27 +17,31 @@ int main () {
     string word;
     int sum = 0;
 
-    ifstream myfile ("/Users/boazraz/Code/CSCI-240/ParsingFileIO/input.txt");
-    ofstream outFile("/Users/boazraz/Code/CSCI-240/ParsingFileIO/sum.txt");
+    ifstream myFile("input.txt");
+    ofstream outFile("sum.txt");
 
-    if (myfile.is_open())
+    if (myFile.is_open())
     {
-        while ( getline (myfile,line, ','))
+        while ( getline (myFile,line, ','))
         {
 
             if (line.empty()) continue;
 
             ss.clear();
             ss.str ("");
-            while (ss << line)      //if not at end of file, continue reading numbers
+            while (ss << line)   // while we read more lines sum the ints and print the words * int
             {
                 ss << line << '\n';
                 ss >> a >> b >> c >> word;
                 sum = a + b +c;
+
+                // This is for testing
                 cout << " this is a: " << a << endl;
                 cout << " this is b: " << b << endl;
                 cout << " this is c: " << c << endl;
                 cout << " sum: " << sum << endl;
+                // end testing print out
+
                 outFile << endl << endl << "Sum of: " << word << ": is "<< sum;
                 outFile << endl;
                 for (int i = 0; i < sum ; ++i) {
@@ -46,16 +50,15 @@ int main () {
 
                 }
 
+            } //
 
-            }
+        } // end while getline
 
-        }
-
-        myfile.close();
+        myFile.close();
         outFile.close();
-    }
+    } // end if file is open
 
     else cout << "Unable to open file";
 
     return 0;
-}
+} // end main
